b15be330c5b423cf7573bcd2d66f0b3a4c49b223
Lautaro Vallejos <lautaro.vallejos123@hotmail.com>
2021-11-01 09:26:39 -0300

    Auth views and unuseful urls was deleteds

--- book/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from book.views import *
-
-urlpatterns = [
-    path('books/', book_view_set),
-    path('authors/', author_view_set),
-    path('library/', library_view_set),
-    path('lead/', lead_view_set),
-]
